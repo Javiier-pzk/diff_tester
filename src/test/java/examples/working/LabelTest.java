74707963ea8f9a65ab0440bc7ec3996e1224344c
Javier <javierphon@gmail.com>
2023-11-28 15:11:15 +0800

    Add passing regression test

--- src/test/java/examples/working/LabelTest.java
+++ src/test/java/examples/working/LabelTest.java
@@ -1,38 +1,43 @@
 package examples.working;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-public class LabelTest {
+class LabelTest {
 
-    @Test
-    void testApply() {
-        String[] includes = {"apple", "banana"};
-        String[] excludes = {"grapefruit"};
+  @Test
+  void applyTest() {
+    // Prepare two Labels, each of which rejects one String and accepts another
+    String rejected1 = "rejected1";
+    String accepted1 = "accepted1";
+    Label label1 = new Label(new String[] {accepted1}, new String[] {rejected1});
 
-        examples.working.Label workingLabel = new examples.working.Label(includes, excludes);
+    String rejected2 = "rejected2";
+    String accepted2 = "accepted2";
+    Label label2 = new Label(new String[] {accepted2}, new String[] {rejected2});
 
-        // These tests should pass with the working code
-        assertTrue(workingLabel.apply("apple"));
-        assertFalse(workingLabel.apply("grapefruit"));
-        assertFalse(workingLabel.apply("orange"));
-    }
+    // Test for label1
+    assertFalse(label1.apply(rejected1)); // rejected String should be removed
+    assertTrue(label1.apply(accepted1)); // included String should pass
 
-    @Test
-    void testApplyWithRegressedCode() {
-        String[] includes = {"apple", "banana"};
-        String[] excludes = {"grapefruit"};
+    // Test for label2
+    assertFalse(label2.apply(rejected2)); // rejected String should be removed
+    assertTrue(label2.apply(accepted2)); // included String should pass
+  }
 
-        examples.regression.Label regressedLabel = new examples.regression.Label(includes, excludes);
-        examples.working.Label workingLabel = new examples.working.Label(includes, excludes);
+  @Test
+  void applyTestWhenIncludeNull() {
+     String rejected = "rejected";
+     Label label = new Label(null, new String[] {rejected});
+     assertFalse(label.apply(rejected)); // rejected String should be removed
+     assertTrue(label.apply("sometext")); // This should pass as include is null
+  }
 
-        // This should pass, as "apple" is included in both versions
-        assertTrue(regressedLabel.apply("apple"));
-
-        // This should fail with the regression version as "grapefruit" is excluded, but regression version will return true
-        assertFalse(regressedLabel.apply("grapefruit"));
-
-        // This test should pass, it's expecting false for non-included "orange" in both versions
-        assertFalse(regressedLabel.apply("orange")); 
-    }
+  @Test
+  void applyTestWhenExcludeNull() {
+    String included = "included";
+    Label label = new Label(new String[] {included}, null);
+    assertTrue(label.apply(included)); // included String should pass
+    assertFalse(label.apply("sometext")); // This should not pass as it's not included
+  }
 }
