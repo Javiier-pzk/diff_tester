7e265f335266c19e4ee17385eb99244c9d248c03
Javier <javierphon@gmail.com>
2024-03-04 21:12:14 +0800

    Reformat file

--- src/test/java/com/tester/evosuite/EvosuiteGeneratorTest.java
+++ src/test/java/com/tester/evosuite/EvosuiteGeneratorTest.java
@@ -7,14 +7,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EvosuiteGeneratorTest {
 
-    @Test
-    void generateWithCmdTest() {
-        EvosuiteGenerator evosuiteGenerator = new EvosuiteGenerator(Conf.EVOSUITE_JAR, Conf.TARGET_EVOSUITE_TESTS_PATH);
-        String res = evosuiteGenerator.generateWithCmd(
-                System.getProperty("user.dir"),
-                "examples.working.Label",
-                "apply(Ljava/lang/String;)Z\""
-        );
-        System.out.println(res);
-    }
+  @Test
+  void generateWithCmdTest() {
+    EvosuiteGenerator evosuiteGenerator = new EvosuiteGenerator(Conf.EVOSUITE_JAR, Conf.TARGET_EVOSUITE_TESTS_PATH);
+    String res = evosuiteGenerator.generateWithCmd(
+            System.getProperty("user.dir"),
+            "examples.working.Label",
+            "apply(Ljava/lang/String;)Z\""
+    );
+    System.out.println(res);
+  }
 }
